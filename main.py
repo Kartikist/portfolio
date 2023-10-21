c9070551e823a4d45032aa1f257cb2560af770d8
Kartik Neti <kartikprajak@gmail.com>
2023-10-21 22:04:51 +0530

    Added descriptions, links

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# st.set_page_config(layout='wide')
+st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
 
@@ -15,13 +15,19 @@ with col2:
     
 st.subheader("This is a subheader. Feel free to contact me from the email mentioned below.")
 
-col3, col4 = st.columns(2)
+col3, empty, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv('data.csv', sep=';')
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
     
