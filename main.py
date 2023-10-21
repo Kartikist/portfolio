f6c4a45b53ca6d0a44c9984b9b39961d24a663fa
Kartik Neti <kartikprajak@gmail.com>
2023-10-21 21:55:50 +0530

    Added columns, names, basic bio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(layout='wide')
+# st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
 
@@ -10,4 +11,17 @@ with col1:
 with col2:
     st.title("Kartik Neti")
     content = """I am GOD"""
-    st.write(content)
+    st.info(content)
+    
+st.subheader("This is a subheader. Feel free to contact me from the email mentioned below.")
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv('data.csv', sep=';')
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+    
